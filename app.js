519f9f8db6c0d4ceaba44ae9cb9a5de471a2cd46
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2022-09-05 18:30:52 +0700

    Tetris Summary
    updated the `freeze()` function, added/reorganized the `div` on the HTML page to move the display box, and I created the `displayShape()` function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score');
   const StartBtn = document.querySelector('#start-button');
   const width = 10;
+  let nextRandom = 0;
 
   // The Tetromineoes
   const lTetromino = [
@@ -54,15 +55,15 @@ document.addEventListener('DOMContentLoaded', () => {
   function draw() {
     current.forEach(index => {
       squares[currentPosition + index].classList.add('tetromino');
-    })
-  }
+    });
+  };
 
   // undraw the Tetromino
   function undraw() {
     current.forEach(index => {
       squares[currentPosition + index].classList.remove('tetromino');
-    })
-  }
+    });
+  };
 
   // make the Tetromino move down every second
   timerId = setInterval(moveDown, 1000);
@@ -72,12 +73,12 @@ document.addEventListener('DOMContentLoaded', () => {
     if(e.keyCode === 37) {
       moveLeft();
     } else if (e.keyCode === 38) {
-      //rotate();
+      rotate();
     } else if (e.keyCode === 39) {
       moveRight();
     } else if (e.keyCode === 40) {
       moveDown();
-    }
+    };
   };
   document.addEventListener('keyup', control);
 
@@ -87,19 +88,21 @@ document.addEventListener('DOMContentLoaded', () => {
     currentPosition += width;
     draw();
     freeze();
-  }
+  };
 
   // freeze function
   function freeze() {
     if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
       current.forEach(index => squares[currentPosition + index].classList.add('taken'));
       // start a new Tetromino falling
-      random = Math.floor(Math.random() * theTetrominoes.length);
+      random = nextRandom;
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length);
       current = theTetrominoes[random][currentRotation];
       currentPosition = 4;
+      draw();
+      displayShape();
+    };
   };
-};
-
 
   // move the Tetromino left, unless it's at the edge or there is a blockage
   function moveLeft() {
@@ -117,17 +120,52 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // move the tetromino right, unless it's at the edge or there is blockage
   function moveRight() {
-    undraw()
+    undraw();
     const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1);
 
     if (!isAtRightEdge) currentPosition += 1;
 
     if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
       currentPosition -= 1;
-    }
+    };
 
     draw();
-  }
+  };
 
+  // rotate the Tetrominio
+  function rotate() {
+    undraw();
+    currentRotation ++
+    if (currentRotation === current.length) { //if the current rotation gets to 4, make it go back to 0
+      currentRotation = 0;
+    };
+    current = theTetrominoes[random][currentRotation];
+    draw();
+  };
+
+  // show up-next Tetromino in mini-grid
+  const displaySquares = document.querySelectorAll('.mini-grid div');
+  const displayWidth = 4;
+  let displayIndex = 0;
+
+  // the Tetromineoes without rotations
+  const upNextTetrominoes = [
+    [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+    [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
+    [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
+    [0, 1, displayWidth, displayWidth+1], //oTetromino
+    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
+  ];
+
+  //display the shape in the mini grid
+  function displayShape() {
+    // remove any trace of tetromino from the entire grid
+    displaySquares.forEach(square => {
+      square.classList.remove('tetromino');
+    });
+    upNextTerominoes[nextRandom].forEach( index => {
+      displaySquares[displayIndex + index].classList.add('tetromino');
+    });
+  };
 
 });

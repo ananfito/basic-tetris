fb8b8b543321b5a9c727a46793735fd35301f48c
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2022-09-12 16:34:49 +0700

    summary
    updated control function in app.js so that deprecated keyCode is removed
    updated HTML and CSS files to include styling

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let timerId;
   let score = 0;
   const colors = [
-    'orange',
+    '#8b4513',
     'red',
     'purple',
     'green',
@@ -83,35 +83,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // assign functions to keyCode
   function control(e) {
-/*
-    // keyCode is deprecated and might not work
-    // below is the replacement, but I can't get it to work (9/06/22)
-    // Documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
-
     switch(e.key) {
-      case "LeftArrow":
+      case "ArrowLeft":
         return moveLeft();
         break;
-      case "UpArrow":
+      case "ArrowUp":
         return rotate();
         break;
-      case "RightArrow":
+      case "ArrowRight":
         return moveRight();
         break;
-      case "DownArrow":
+      case "ArrowDown":
         return moveDown();
         break;
-    }
-    */
-
-    if (e.keyCode === 37) {
-      moveLeft();
-    } else if (e.keyCode === 38) {
-      rotate();
-    } else if (e.keyCode === 39) {
-      moveRight();
-    } else if (e.keyCode === 40) {
-      moveDown();
     };
   };
 
@@ -148,13 +132,10 @@ document.addEventListener('DOMContentLoaded', () => {
   function moveLeft() {
     undraw();
     const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0);
-
     if(!isAtLeftEdge) currentPosition -=1;
-
     if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
       currentPosition +=1;
     };
-
     draw();
   };
 
@@ -163,14 +144,36 @@ document.addEventListener('DOMContentLoaded', () => {
   function moveRight() {
     undraw();
     const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1);
-
     if (!isAtRightEdge) currentPosition += 1;
-
     if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
       currentPosition -= 1;
     };
-
     draw();
+  };
+
+
+    ///FIX ROTATION OF TETROMINOS A THE EDGE
+  function isAtRight() {
+    return current.some(index=> (currentPosition + index + 1) % width === 0)
+  };
+
+  function isAtLeft() {
+    return current.some(index=> (currentPosition + index) % width === 0)
+  };
+
+  function checkRotatedPosition(P){
+    P = P || currentPosition       //get current position.  Then, check if the piece is near the left side.
+    if ((P+1) % width < 4) {         //add 1 because the position index can be 1 less than where the piece is (with how they are indexed).
+      if (isAtRight()) {            //use actual position to check if it's flipped over to right side
+        currentPosition += 1    //if so, add one to wrap it back around
+        checkRotatedPosition(P) //check again.  Pass position from start, since long block might need to move more.
+      };
+    } else if (P % width > 5) {
+      if (isAtLeft()){
+        currentPosition -= 1
+      checkRotatedPosition(P)
+      };
+    };
   };
 
 
@@ -234,9 +237,8 @@ document.addEventListener('DOMContentLoaded', () => {
   function addScore() {
     for (let i = 0; i < 199; i += width) {
       const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9];
-
       if (row.every(index => squares[index].classList.contains('taken'))) {
-        score +=10;
+        score +=100;
         scoreDisplay.innerHTML = score;
         row.forEach(index => {
           squares[index].classList.remove('taken');
